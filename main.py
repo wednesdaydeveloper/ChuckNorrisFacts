efa627b1b6fca707018cd65fc2a3aa39773c2a0f
OKAMOTO Takashi <wednesday.developer@gmail.com>
2021-03-04 07:31:28 +0000

    リファクタリング。

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import os
-import requests
 from flask import abort
 from linebot import (
     LineBotApi, WebhookParser
@@ -29,30 +28,72 @@ http = urllib3.PoolManager()
 translate_client = translate.Client()
 
 def chuckNorrisFacts(request):
+    """
+    エントリポイント
+
+    Parameters
+    ----------
+    request
+            リクエスト
+    """
+
     signature = request.headers['X-Line-Signature']
     body = request.get_data(as_text=True)
     try:
         events = parser.parse(body, signature)
+        for event in events:
+            send_line_text_message(event.reply_token)
+            break
     except InvalidSignatureError:
         abort(400)
-    # for logging request contents
-    print(events)
-    for event in events:
-        send_line_text_message(event.reply_token, event.message.text)
-        break
     return 'OK'
 
-def send_line_text_message(reply_token, text_message):
+def send_line_text_message(reply_token):
+    """
+    LINE に 取得したChuck Norris Facts の英語と日本語のメッセージを返す。
+
+    Parameters
+    ----------
+    reply_token
+            reply_token
+    """
+
+    print(f'loading chuck norris facts...')
     joke_en = get_joke()
-    joke_ja = translate_text(joke_en)
+    print(f'translating chuck norris facts...')
+    joke_ja = get_japanese_text(joke_en)
+    print(f'english: {joke_en}')
+    print(f'japanese: {joke_ja}')
     line_bot_api.reply_message(reply_token, [TextSendMessage(text=joke_en), TextSendMessage(text=joke_ja)])
 
 def get_joke():
+    """
+    Chuck Norris Facts の英語のメッセージを返す。
+
+    Returns
+    -------
+    english_message : string
+        Chuck Norris Facts の英語のメッセージ。
+    """
+
     resp = http.request('GET', URL)
     data = json.loads(resp.data.decode('utf-8'))
     return data['value']
 
-def translate_text(english_text):
+def get_japanese_text(english_text):
+    """
+    Chuck Norris Facts の英語のメッセージから、日本語のメッセージを作成して返す。
+
+    Parameters
+    ----------
+    english_text
+            Chuck Norris Facts の英語のメッセージ
+    Returns
+    -------
+    japanese_message : string
+        Chuck Norris Facts の日本語のメッセージ。
+    """
+
     target="ja"
     result = translate_client.translate(english_text, target_language=target)
     return result["translatedText"]
